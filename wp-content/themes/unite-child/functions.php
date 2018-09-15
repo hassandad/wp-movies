69e5660490d3437b6b5fe2ae60e876ffcfd31f06
Hassan Dad Khan <hassan_dad_khan@hotmail.com>
2018-09-15 15:30:02 +0500

    Add Custom fields with the help of plugin named Advanced custom fields. And also fixed countries taxonomy

--- wp-content/themes/unite-child/functions.php
+++ wp-content/themes/unite-child/functions.php
@@ -59,37 +59,31 @@ function create_film_taxonomies() {
 
 	register_taxonomy( 'genre', array( 'cl_films' ), $args );
 
-	// Add new taxonomy, NOT hierarchical (like tags)
+	// Add new taxonomy, make it hierarchical (like categories)
 	$labels = array(
-		'name'                       => _x( 'Countries', 'taxonomy general name', 'textdomain' ),
-		'singular_name'              => _x( 'Country', 'taxonomy singular name', 'textdomain' ),
-		'search_items'               => __( 'Search Countries', 'textdomain' ),
-		'popular_items'              => __( 'Popular Countries', 'textdomain' ),
-		'all_items'                  => __( 'All Countries', 'textdomain' ),
-		'parent_item'                => null,
-		'parent_item_colon'          => null,
-		'edit_item'                  => __( 'Edit Country', 'textdomain' ),
-		'update_item'                => __( 'Update Country', 'textdomain' ),
-		'add_new_item'               => __( 'Add New Country', 'textdomain' ),
-		'new_item_name'              => __( 'New Country Name', 'textdomain' ),
-		'separate_items_with_commas' => __( 'Separate countries with commas', 'textdomain' ),
-		'add_or_remove_items'        => __( 'Add or remove countries', 'textdomain' ),
-		'choose_from_most_used'      => __( 'Choose from the most used countries', 'textdomain' ),
-		'not_found'                  => __( 'No countries found.', 'textdomain' ),
-		'menu_name'                  => __( 'Countries', 'textdomain' ),
+		'name'              => _x( 'Countries', 'taxonomy general name', 'textdomain' ),
+		'singular_name'     => _x( 'Country', 'taxonomy singular name', 'textdomain' ),
+		'search_items'      => __( 'Search Countries', 'textdomain' ),
+		'all_items'         => __( 'All Countries', 'textdomain' ),
+		'parent_item'       => __( 'Parent Country', 'textdomain' ),
+		'parent_item_colon' => __( 'Parent Country:', 'textdomain' ),
+		'edit_item'         => __( 'Edit Country', 'textdomain' ),
+		'update_item'       => __( 'Update Country', 'textdomain' ),
+		'add_new_item'      => __( 'Add New Country', 'textdomain' ),
+		'new_item_name'     => __( 'New Country Name', 'textdomain' ),
+		'menu_name'         => __( 'Country', 'textdomain' ),
 	);
 
 	$args = array(
-		'hierarchical'          => false,
-		'labels'                => $labels,
-		'show_ui'               => true,
-		'show_admin_column'     => true,
-		'update_count_callback' => '_update_post_term_count',
-		'query_var'             => true,
-		'rewrite'               => array( 'slug' => 'country' ),
+		'hierarchical'      => true,
+		'labels'            => $labels,
+		'show_ui'           => true,
+		'show_admin_column' => true,
+		'query_var'         => true,
+		'rewrite'           => array( 'slug' => 'country' ),
 	);
 
-	register_taxonomy( 'country', 'cl_films', $args );
+	register_taxonomy( 'country', array( 'cl_films' ), $args );
         
         // Add new taxonomy, make it hierarchical (like categories)
 	$labels = array(
